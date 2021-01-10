2e7573cf34608ebcf67f0d745efaf42076534008
jason <jason@DESKTOP-7P9LTQS>
2021-01-10 21:49:12 +0000

    Working on home PC. Adding cookies to save and load progress

--- Leverage/wwwroot/site.js
+++ Leverage/wwwroot/site.js
@@ -11,6 +11,32 @@ var newSection;
 var currentSection = 'left';
 var lastSection = 'lastSectionDefault';
 
+function writeCookie(name, value) {
+    document.cookie = name + "=" + value + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function deleteCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 function drawSVG(p, dotNetObject, addPower) {
     // line to center
     line.attr("x1", p[0]).attr("y1", p[1]).attr("x2", base.cx.baseVal.value).attr("y2", base.cy.baseVal.value - 10);
